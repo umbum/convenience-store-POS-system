fb2e57f5703d9cb0e4597b7d52a86950d1b61164
umbum <umbum7601@gmail.com>
2019-05-24 23:29:05 +0900

    static resources 및 예제 index page 추가

---
+++ project/src/main/java/com/umbum/pos/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.umbum.pos.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class IndexController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
+
