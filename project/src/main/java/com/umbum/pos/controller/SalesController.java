e99cca7654d1402068c62a0d3b9b742e5b987a07
umbum <umbum7601@gmail.com>
2019-05-26 19:38:03 +0900

    sales 페이지 layout 잡기

--- project/src/main/java/com/umbum/pos/controller/SalesController.java
+++ project/src/main/java/com/umbum/pos/controller/SalesController.java
@@ -1,4 +1,21 @@
 package com.umbum.pos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/sales")
 public class SalesController {
+    @RequestMapping(method= RequestMethod.GET)
+    public String sales() {
+        return "sales.html";
+    }
+
+    @RequestMapping("/datatable")
+    public String salesDataTable() {
+        return "sales_datatable.html";
+    }
+
 }
